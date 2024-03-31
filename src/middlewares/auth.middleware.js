eb60d82bf89be5b4090012813b63b95ee2c28080
Abuzar <abuzar0561446@gmail.com>
2024-03-31 06:02:18 +0530

    Add VerifyToken endpoint

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const ApiError = require("../utils/ApiError.js");
+const wrapAsyncHandler = require("../utils/wrapAsyncHandler.js");
+const User = require("../models/user.model.js");
+
+const verifyJWT = wrapAsyncHandler(async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+    const token =req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+      console.log(token);
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request jsonweb token not found");
+    }
+    const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+     const user= User.findById(decodedToken?._id).select("-password -refreshToken");
+     if(!user){
+      throw new ApiError(404,"Invalid Access Token, User not found!");
+     }
+     req.user= user;
+      next();
+  } catch (error) {
+    throw new ApiError(404, error?.message || "Invalid access Token");
+  }
+});
+
+module.exports=verifyJWT
